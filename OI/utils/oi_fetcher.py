e889ab8e99783cf17c3fae74d40b46083cda019f
Ford Pickert <fordpickert@gmail.com>
2023-04-11 13:24:17 -0500

    data pulling from API correctly

--- OI/utils/oi_fetcher.py
+++ OI/utils/oi_fetcher.py
@@ -7,19 +7,30 @@ load_dotenv()
 COINALYZE = os.getenv('COINALYZE_KEY')
 
 # Query Params
-symbols = str('BTCUSDT')
+symbols = "BTCUSDT_PERP.0,ETHUSDT_PERP.0,MATICUSDT_PERP.0,FTMUSDT_PERP.0,SOLUSDT_PERP.0,ARBUSDT_PERP.0,"
 
 async def fetch(client):
-    async with client.get(f'https://api.coinalyze.net/v1/open-interest?symbols=BTCUSDT&api_key={COINALYZE}') as res:
+    async with client.get(f"https://api.coinalyze.net/v1/open-interest?symbols={symbols}&api_key={COINALYZE}") as res:
         assert res.status == 200
-        return await res.text()
+        return await res.json()
 
 
 async def main():
+    print("Grabbing OI data from Coinalyze...")
 
     async with aiohttp.ClientSession() as client:
         data = await fetch(client)
-        print(data)
+
+        result = []
+
+        for key in data:
+            for item in key.items():
+                # print(item[1])
+                result.append(item[1])
+
+
+        print(result)
+        return result
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
